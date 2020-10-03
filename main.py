06977c7d23c7f4cf903fb843ee7189e87e6711a3
Catherine <catherineep4189@gmail.com>
2020-10-03 21:20:09 +0100

    input questions

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 def tech_in_the_90s():
     dial_a_friend = input('Whose your Chuckie Finister to your Tommy Pickles? ')
 
-    print(dial_a_friend)
+    bingo = int(input('Lucky number... '))
+
+    favourite_limb = input('We all have them but tell me your favourite body part! ')
+
+    anywhere_but_the_gaff = input('One day I was lost in... ')
+
+    adjective = input('If Daria Morgendorffer was bespectacled then what best describes the 90s you? ')
+
+    missing_limb = input('From Lestat to Hannibal, they all had a body part vice or two, name another one that you\'re scared to lose... ')
+
+    plural_noun1 = input('You can\'t eat one dorito, just doesn\'t make sense...Name things that just better work as pairs or more... ')
+
+    plural_noun2 = input('Mum, I can\'t go to see Nirvana in one Doc Marten!!! Name any iconic group - FYI doesn\'t have to be music related... ')
+
+    one_afternoon_at_the_bookies = int(input('I remember when my gran use to leave me outside the bookies for 2 minutes to place a bet on the horse Red Rum, how many quid you putting on then? '))
 
 tech_in_the_90s()

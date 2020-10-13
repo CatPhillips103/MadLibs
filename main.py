270cf5f721a2912033e69c65cfc430020c5ad90d
Catherine <catherineep4189@gmail.com>
2020-10-13 16:08:38 +0100

    input values parsed through f-strings

--- main.py
+++ main.py
@@ -7,14 +7,25 @@ def tech_in_the_90s():
 
     anywhere_but_the_gaff = input('One day I was lost in... ')
 
-    adjective = input('If Daria Morgendorffer was bespectacled then what best describes the 90s you? ')
+    describe_me = input('If Daria Morgendorffer was bespectacled then what best describes the 90s you? ')
 
     missing_limb = input('From Lestat to Hannibal, they all had a body part vice or two, name another one that you\'re scared to lose... ')
 
-    plural_noun1 = input('You can\'t eat one dorito, just doesn\'t make sense...Name things that just better work as pairs or more... ')
+    more_than_one1 = input('You can\'t eat one dorito, just doesn\'t make sense...Name things that just better work as pairs or more... ')
 
-    plural_noun2 = input('Mum, I can\'t go to see Nirvana in one Doc Marten!!! Name any iconic group - FYI doesn\'t have to be music related... ')
+    more_than_one2 = input('Mum, I can\'t go to see Nirvana in one Doc Marten!!! Name any iconic group - FYI doesn\'t have to be music related... ')
 
     one_afternoon_at_the_bookies = int(input('I remember when my gran use to leave me outside the bookies for 2 minutes to place a bet on the horse Red Rum, how many quid you putting on then? '))
+
+
+    madlib1 = f'This week only, {dial_a_friend}\'s Discount Tech is holding a/an {bingo} percent off sale! Get Your {favourite_limb} on the coolest gadgets of the 1990s.'
+
+    madlib2 = f'Don\'t get caught walking around {anywhere_but_the_gaff} without this {describe_me} portable CD player. Batteries and {missing_limb}-phones not included.'
+
+    madlib3 = f'{more_than_one1}: Save all your {more_than_one2} on these storage discs that hold up to {one_afternoon_at_the_bookies} megabytes'
+
+    print(f'\n{madlib1}\n')
+    print(f'{madlib2}\n')
+    print(f'{madlib3}\n')
 
 tech_in_the_90s()
